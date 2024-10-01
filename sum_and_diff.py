eb24c2ddd64330dc2b97a137b48dd40bf5968896
Ryua0105 <0101jcj@gmail.com>
2024-10-01 17:18:19 +0900

    Add sum and diff calculation program

--- sum_and_diff.py
+++ sum_and_diff.py
@@ -3,10 +3,13 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> num1 = int(input("첫 번째 정수를 입력하세요: "))
 첫 번째 정수를 입력하세요: 10
 >>> num2 = int(input("두 번째 정수를 입력하세요: "))
-두 번째 정수를 입력하세요: 4
+두 번째 정수를 입력하세요: 5
+>>> sum_result = num1 + num2
+... difference = num1 - num2
+SyntaxError: multiple statements found while compiling a single statement
 >>> sum_result = num1 + num2
 >>> difference = num1 - num2
 >>> print("두 정수의 합: ", sum_result)
-두 정수의 합:  14
->>> print("두 정수의 차: ", difference)
-두 정수의 차:  6
+두 정수의 합:  15
+>>> print("두 정수의 합: ", difference)
+두 정수의 합:  5
