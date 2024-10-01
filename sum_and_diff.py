f78156c82cbb8420b342dd25f55765096d3d8994
Ryua0105 <0101jcj@gmail.com>
2024-10-01 17:11:16 +0900

    Add sum and diff calculation program

---
+++ sum_and_diff.py
@@ -0,0 +1,12 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = int(input("첫 번째 정수를 입력하세요: "))
+첫 번째 정수를 입력하세요: 10
+>>> num2 = int(input("두 번째 정수를 입력하세요: "))
+두 번째 정수를 입력하세요: 4
+>>> sum_result = num1 + num2
+>>> difference = num1 - num2
+>>> print("두 정수의 합: ", sum_result)
+두 정수의 합:  14
+>>> print("두 정수의 차: ", difference)
+두 정수의 차:  6
